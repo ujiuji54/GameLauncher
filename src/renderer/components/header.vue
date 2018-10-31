caa4fb3322bef2f6344019885f52fc6485a2e7b3
ujiuji54 <r114306@g.kobe-kosen.ac.jp>
2018-10-31 14:38:04 +0900

    header not found

---
+++ src/renderer/components/header.vue
@@ -0,0 +1,12 @@
+<template>
+  <header class="nav">
+    <div class="nav-right">
+      <span class="nav-item">
+        <div class="control has-addons">
+          <input class="input" type="search" name="search" placeholder="キーワード検索">
+          <a class="button is-info"><i class="fa fa-search"></i>検索</a>
+        </div>
+      </span>
+    </div>
+  </header>
+</template>
--- src/renderer/components/index.vue
+++ src/renderer/components/index.vue
@@ -1,11 +1,28 @@
 <template>
   <div>
     <router-link to='/'>Hello</router-link>
+    <header></header>
   </div>
 </template>
 
 <script>
+  import Header from './components/header'
+
+  const remote = require('electron').remote
+  const app = remote.app
+  const path = app.getAppPath()
+
+  console.log(path)
+
+  const exec = require('child_process').execFile
+
   export default {
-    name: 'index'
+    name: 'index',
+    components: {
+      Header
+    },
+    method: {
+      exec: exec(path + '/', {}, function () {})
+    }
   }
 </script>
