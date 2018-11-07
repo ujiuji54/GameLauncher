3be6d86fa8e216f3b6da8ab887b57cc6c65623ee
ujiuji54 <r114306@g.kobe-kosen.ac.jp>
2018-11-08 00:20:29 +0900

    file一覧取得部分作成中

--- src/renderer/components/index.vue
+++ src/renderer/components/index.vue
@@ -11,6 +11,22 @@
   import FooterItem from './FooterItem'
   import App from './App'
 
+  const fs = require('fs')
+  const path = require('path')
+  const remote = require('electron').remote
+  const app = remote.app
+  const exeDirPath = path.dirname(app.getPath('exe'))
+  console.log(exeDirPath)
+
+  var fileList = []
+  fs.readdir(exeDirPath, function (err, files) {
+    if (err) throw err
+    fileList = files.filter(function (file) {
+      return fs.statSync(file).isFile() && /.*\.json$/.test(file)
+    })
+  })
+  console.log(fileList)
+
   export default {
     name: 'index',
     components: {
